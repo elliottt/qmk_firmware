a02234da459b999cd2af33a018ba9f669ac17e47
Trevor Elliott <awesomelyawesome@gmail.com>
2020-02-19 23:02:10 -0800

    My keyboard configs

---
+++ keyboards/ergodox_infinity/keymaps/elliottt/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#undef  IGNORE_MOD_TAP_INTERRUPT
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#undef PERMISSIVE_HOLD
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 200
---
+++ keyboards/gboards/gergo/keymaps/elliottt/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#undef PERMISSIVE_HOLD
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 200
