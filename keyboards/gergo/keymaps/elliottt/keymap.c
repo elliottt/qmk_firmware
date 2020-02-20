de947f2606399f5f88944235851374579697b1f8
Trevor Elliott <awesomelyawesome@gmail.com>
2020-02-19 23:28:50 -0800

    Make some changes to make the mouse easier to use.

--- keyboards/gergo/keymaps/elliottt/keymap.c
+++ keyboards/gergo/keymaps/elliottt/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------'                      `--------------'
  */
 [BASE] = LAYOUT_gergo(
-    LT(NUMB, KC_EQUAL),     KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                            KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_BSLS,
-    MT(MOD_LCTL, KC_TAB),   KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_ESC,                          KC_ESC,   KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, MT(MOD_RCTL, KC_QUOT),
-    KC_LSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_LGUI, KC_BTN3,       KC_PGDN, KC_RGUI,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(KC_RSFT, KC_MINUS),
-      MT(MOD_LGUI, KC_DEL), MT(MOD_LALT, KC_ENT), LT(NUMB, KC_BSPC), LT(SYMB, KC_DEL), LT(SYMB, KC_ENT), LT(NUMB, KC_SPC), KC_TAB, KC_BSPC
+    LT(NUMB, KC_EQUAL),   KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                              KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_BSLS,
+    MT(MOD_LCTL, KC_TAB), KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_ESC,                            KC_ESC,   KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, MT(MOD_RCTL, KC_QUOT),
+    KC_LSFT,              KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_LGUI, KC_BTN3,         KC_PGDN, KC_RGUI,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    MT(MOD_LGUI, KC_DEL), MT(MOD_LALT, KC_ENT), LT(NUMB, KC_BSPC), LT(SYMB, KC_DEL),   LT(SYMB, KC_ENT), LT(NUMB, KC_SPC), KC_TAB, KC_BSPC
     ),
 /* Keymap 1: Symbols layer
  *
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
  * |        |  #   |  $   |  (   |  )   |  `   |      |           |      |   +  |  -   |  /   |  {   |  }   |  ' "   |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |        |  %   |  ^   |  [   |  ]   |  ~   |      |           |      |   &  |  =   |  ,   |  .   |  / ? | - _    |
+ * |        |  ~   |  ^   |  [   |  ]   |  ~   |      |           |      |   &  |  =   |  ,   |  .   |  / ? | - _    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *                        .------.   .------.                                 .------.   .-----.
  *                        |      |   |      |                                 |      |   | DEL |
@@ -77,9 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                         ,-------------------------------------------.
  * |        |   1  |  2   |  3   |  4   |  5   |                         |  6   |  7   |  8   |  9   |  0   |  _ -   |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |  F1    |  F2  | F3   | F4   | F5   | F6   | BTN1 |           |      | LEFT | DOWN |  UP  | RIGHT|VolDn | VolUp  |
+ * |  F1    |  F2  | F3   | F4   | F5   | F6   | BTN1 |           | BTN1 | LEFT | DOWN |  UP  | RIGHT|VolDn | VolUp  |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |  F7    |  F8  | F9   | F10  | F11  | F12  | BTN2 |           |      | MLFT | MDWN | MUP  | MRGHT|Ply/Pa|  Skip  |
+ * |  F7    |  F8  | F9   | F10  | F11  | F12  | BTN2 |           | BTN2 | MLFT | MDWN | MUP  | MRGHT|Ply/Pa|  Skip  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *                        .------.   .------.                                 .------.   .-----.
  *                        |      |   |      |                                 |      |   |     |
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [NUMB] = LAYOUT_gergo(
     KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS,
-    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
-    KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     KC_PGUP, KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_BTN1,                       KC_BTN1,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
+    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN2, KC_TRNS,     KC_PGUP, KC_BTN2,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS
     ),
 };
