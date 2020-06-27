9366db3737afc740cf4a68c7aa20a7c393978355
Trevor Elliott <awesomelyawesome@gmail.com>
2020-06-27 10:49:41 -0700

    Small tweaks to the gergo layout

--- keyboards/gergo/keymaps/elliottt/keymap.c
+++ keyboards/gergo/keymaps/elliottt/keymap.c
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbols layer
  *
  * ,-------------------------------------------.                         ,-------------------------------------------.
- * |  +     |  !   |  @   |  #   |  $   |  %   |                         |   ^  |  &   |  *   |  (   |  )   |  - _   |
+ * |        |  !   |  @   |  #   |  $   |  %   |                         |   ^  |  &   |  *   |  (   |  )   |  - _   |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |  |   |  \   |  (   |  )   |  `   |      |           |      |   +  |  -   |  :   |  {   |  }   |  ' "   |
+ * |        |  |   |  (   |  )   |  ~   |  `   |      |           |      |   +  |  -   |  :   |  {   |  }   |  ' "   |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |        |  `   |  ~   |  [   |  ]   |  ~   |      |           |      |   &  |  =   |  ,   |  [   |  ]   |        |
+ * |        |  \   |  [   |  ]   |      |      |      |           |      |   &  |  =   |  ,   |  [   |  ]   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *                        .------.   .------.                                 .------.   .-----.
  *                        |      |   |      |                                 |      |   |     |
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_gergo(
     KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-    KC_TRNS, KC_PIPE, KC_BSLS, KC_LPRN, KC_RPRN, KC_GRV,  KC_TRNS,                          KC_TRNS, KC_PLUS, KC_MINS, KC_COLN, KC_LCBR, KC_RCBR, KC_QUOT,
-    KC_TRNS, KC_GRV,  KC_TILD, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS, KC_TRNS,        KC_PGUP, KC_TRNS, KC_AMPR, KC_EQL,  KC_COMM, KC_LBRC, KC_RBRC, KC_TRNS,
+    KC_TRNS, KC_PIPE, KC_LPRN, KC_RPRN, KC_TILD, KC_GRV,  KC_TRNS,                          KC_TRNS, KC_PLUS, KC_MINS, KC_COLN, KC_LCBR, KC_RCBR, KC_QUOT,
+    KC_TRNS, KC_BSLS, KC_TILD, KC_LBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_PGUP, KC_TRNS, KC_AMPR, KC_EQL,  KC_COMM, KC_LBRC, KC_RBRC, KC_TRNS,
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 /* Keymap 2: Pad/Function layer
