6d4b06b073740669ecec7d01a64547f738860db0
Trevor Elliott <awesomelyawesome@gmail.com>
2020-06-27 10:57:58 -0700

    Add missing symbol layer button

--- keyboards/ergodox_infinity/keymaps/elliottt/keymap.c
+++ keyboards/ergodox_infinity/keymaps/elliottt/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_ESC,
         MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LGUI,
-        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, MO(SYMB),
                                                                     KC_TRNS, KC_TRNS,
                                                                              KC_TRNS,
                                                  LT(NUMB, KC_BSPC), KC_DEL,  KC_TRNS,
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     | NUMB|
  *    +-----+-----+-----+
  */
-             KC_TRNS, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_TRNS,
-             KC_ESC,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_MINS,
-                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  MT(MOD_RCTL, KC_QUOT),
-             KC_RGUI, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+             KC_TRNS, KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     KC_TRNS,
+             KC_ESC,  KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS,
+                      KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN,  MT(MOD_RCTL, KC_QUOT),
+             KC_RGUI, KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                              MO(SYMB), KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
              KC_TRNS, KC_TRNS,
              KC_TRNS,
              KC_TRNS, KC_ENT, LT(NUMB, KC_SPC)
