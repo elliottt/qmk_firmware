06b02f261997a829f6b62590f57a8ee6ea7abd3f
Trevor Elliott <awesomelyawesome@gmail.com>
2020-08-24 15:29:45 -0700

    Experiment with placement of esc, ctrl, and gui

--- keyboards/ergodox_infinity/keymaps/elliottt/keymap.c
+++ keyboards/ergodox_infinity/keymaps/elliottt/keymap.c
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                  | NUMB|     |     |
  *                                  +-----+-----+-----+
  */
-        KC_TRNS,              KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_TRNS,
-        MT(MOD_LALT, KC_EQL), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_ESC,
-        MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LGUI,
-        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, MO(SYMB),
+        KC_TRNS,              KC_1,    KC_2,    KC_3,               KC_4,               KC_5, KC_TRNS,
+        MT(MOD_LALT, KC_EQL), KC_Q,    KC_W,    KC_E,               KC_R,               KC_T, KC_ESC,
+        MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    MT(MOD_LGUI, KC_D), MT(MOD_LCTL, KC_F), KC_G,
+        KC_LSFT,              KC_Z,    KC_X,    KC_C,               KC_V,               KC_B, KC_LGUI,
+        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS,            MO(SYMB),
                                                                     KC_TRNS, KC_TRNS,
                                                                              KC_TRNS,
                                                  LT(NUMB, KC_BSPC), KC_DEL,  KC_TRNS,
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     | NUMB|
  *    +-----+-----+-----+
  */
-             KC_TRNS, KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     KC_TRNS,
-             KC_ESC,  KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     MT(MOD_LALT, KC_MINS),
-                      KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN,  MT(MOD_RCTL, KC_QUOT),
-             KC_RGUI, KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                              MO(SYMB), MO(FUN), KC_TRNS, KC_TRNS,  KC_TRNS,
+             KC_TRNS, KC_6,   KC_7,               KC_8,               KC_9,    KC_0,     KC_TRNS,
+             KC_ESC,  KC_Y,   KC_U,               KC_I,               KC_O,    KC_P,     MT(MOD_LALT, KC_MINS),
+                      KC_H,   MT(MOD_RCTL, KC_J), MT(MOD_RGUI, KC_K), KC_L,    KC_SCLN,  MT(MOD_RCTL, KC_QUOT),
+             KC_RGUI, KC_N,   KC_M,               KC_COMM,            KC_DOT,  KC_SLSH,  KC_RSFT,
+                              MO(SYMB),           MO(FUN),            KC_TRNS, KC_TRNS,  KC_TRNS,
              KC_TRNS, KC_TRNS,
              KC_TRNS,
              KC_TRNS, KC_ENT, LT(NUMB, KC_SPC)
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |     |     |
  *                                  +-----+-----+-----+
  *                                  |     |     |     |
- *                                  |     |     +-----+
+ *                                  | ESC |     +-----+
  *                                  |     |     |     |
  *                                  +-----+-----+-----+
  */
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                       KC_TRNS, KC_TRNS,
                                                                KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,
+                                              KC_ESC, KC_TRNS, KC_TRNS,
 /* right hand
  *        +-----+-----+-----+-----+-----+-----+-------+
  *        | f12 | f6  | f7  | f8  | f9  | f10 |       |
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     |
  *    +-----+-----+-----+
  *    |     |     |     |
- *    +-----+     |     |
+ *    +-----+     | ESC |
  *    |     |     |     |
  *    +-----+-----+-----+
  */
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_TRNS, KC_TRNS, KC_ESC
 ),
 [NUMB] = LAYOUT_ergodox( // numbers
 /* Left hand
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |     |     |
  *                                  +-----+-----+-----+
  *                                  |     |     |     |
- *                                  |     |     +-----+
+ *                                  | ESC |     +-----+
  *                                  |     |     |     |
  *                                  +-----+-----+-----+
  */
@@ -157,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+                                   KC_ESC, KC_TRNS, KC_TRNS,
 /* right hand
  *        +-----+-----+-----+-----+-----+-----+-------+
  *        |     |     |     |     |     |     |       |
@@ -173,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     |
  *    +-----+-----+-----+
  *    |     |     |     |
- *    +-----+     |     |
+ *    +-----+     | ESC |
  *    |     |     |     |
  *    +-----+-----+-----+
  */
@@ -184,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, RESET,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_TRNS, KC_TRNS, KC_ESC
 ),
 [FUN] = LAYOUT_ergodox( // function keys
 /* Left hand
