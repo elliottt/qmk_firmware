afdef821c132e7ea02ccf5ad51e0fe3d31491a35
Trevor Elliott <awesomelyawesome@gmail.com>
2021-04-25 14:37:37 -0700

    Move numbers to the left hand

--- keyboards/handwired/tightyl/keymaps/default/keymap.c
+++ keyboards/handwired/tightyl/keymaps/default/keymap.c
@@ -28,6 +28,7 @@ enum layer_names {
     BASE,
     SYM,
     NUMB,
+    NAV,
     FUN,
 };
 
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL,         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,
     LCTL_T(KC_TAB), LCTL_T(KC_A), KC_S,         KC_D,         KC_F,         KC_G,
     KC_LSFT,        KC_Z,         LALT_T(KC_X), LSFT_T(KC_C), LGUI_T(KC_V), KC_B,
-                                  KC_TRNS,      MO(FUN),      MO(SYM),      LT(NUMB, KC_BSPC), KC_DEL,
+                                  KC_TRNS,      MO(FUN),      MO(SYM),      LT(NAV, KC_BSPC), KC_DEL,
 /* right hand
  *        +-----+-----+-----+-----+-----+-------+
  *        |  Y  |  U  |  I  |  O  |  P  |-/RALT |
@@ -69,6 +70,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [NUMB] = LAYOUT_tightyl(
+// left hand
+//  +-------+-----+-----+-----+-----+-----+
+//  |       |  1  |  2  |  3  |  4  |  5  |
+//  +-------+-----+-----+-----+-----+-----+
+//  |       |  6  |  7  |  8  |  9  |  0  |
+//  +-------+-----+-----+-----+-----+-----+
+//  |       |     |     |     |     |     |
+//  +-------+-----+-----+-----+-----+-----+-----+
+//                |     |     |     |     |     |
+//                +-----+-----+-----+-----+-----+
+
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+    KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+// right hand
+//        +-----+-----+-----+-----+-----+-------+
+//        |  6  |  7  |  8  |  9  |  0  |       |
+//        +-----+-----+-----+-----+-----+-------+
+//        |     |     |     |     |     |       |
+//        +-----+-----+-----+-----+-----+-------+
+//        |     |     |     |     |     |       |
+//  +-----+-----+-----+-----+-----+-----+-------+
+//  |     |     |     |     |     |
+//  +-----+-----+-----+-----+-----+
+             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
+    [NAV] = LAYOUT_tightyl(
 // left hand
 //  +-------+-----+-----+-----+-----+-----+
 //  |       |  1  |  2  |  3  |  4  |  5  |
@@ -99,6 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
+
 
     [SYM] = LAYOUT_tightyl(
 // left hand
